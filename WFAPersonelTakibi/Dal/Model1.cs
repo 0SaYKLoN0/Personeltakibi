07a4ad58bb6eb62b224b8bc55d2ebc70fe7e69a8
0SaYKLoN0 <ufuk.meco@gmail.com>
2019-09-21 13:37:44 +0300

    Add project files.

---
+++ WFAPersonelTakibi/Dal/Model1.cs
@@ -0,0 +1,21 @@
+namespace WFAPersonelTakibi.Dal
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using WFAPersonelTakibi.Models;
+
+    public class Model1 : DbContext
+    {
+
+        public Model1()
+            : base("name=Model1")
+        {
+
+        }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+    }
+
+
+}
---
+++ WFAPersonelTakibi/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace WFAPersonelTakibi.Models
+{
+    
+        public enum Gender
+        {
+            Male,Female,Karışık
+        }
+    }
